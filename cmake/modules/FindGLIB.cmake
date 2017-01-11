a5115a7018867ad1eaa87c6fe5f243a08e472fc6
Chun-wei Fan <fanchunwei@src.gnome.org>
2017-01-11 17:41:45 +0800

    CMake: Enable GTK/GLib detection on Windows
    Use the logic in FindCairo.cmake so that we can build poppler-glib with
    the GTK-3.x test/demo programs, even when pkg-config is not available.

--- cmake/modules/FindGLIB.cmake
+++ cmake/modules/FindGLIB.cmake
@@ -11,7 +11,13 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-include(FindPackageHandleStandardArgs)
+if(GLIB2_INCLUDE_DIRS AND GLIB2_LIBRARIES)
+
+  # in cache already
+  set(GLIB2_FOUND TRUE)
+
+else(GLIB2_INCLUDE_DIRS AND GLIB2_LIBRARIES)
+  include(FindPackageHandleStandardArgs)
 
 if (NOT WIN32)
   find_package(PkgConfig REQUIRED)
@@ -20,4 +26,42 @@ if (NOT WIN32)
 
   find_package_handle_standard_args(GLib DEFAULT_MSG GLIB2_LIBRARIES GLIB2_CFLAGS)
 
+else(NOT WIN32)
+  # assume so, for now
+  set(GLIB2_FOUND TRUE)
+
 endif(NOT WIN32)
+
+if(GLIB2_FOUND)
+  # set it back as false
+  set(GLIB2_FOUND FALSE)
+
+  find_library(GLIB2_LIBRARY glib-2.0
+               HINTS ${_pc_glib_LIBRARY_DIRS}
+  )
+
+  find_library(GOBJECT2_LIBRARY gobject-2.0
+               HINTS ${_pc_gobject_LIBRARY_DIRS}
+  )
+
+  find_library(GIO2_LIBRARY gio-2.0
+               HINTS ${_pc_gio_LIBRARY_DIRS}
+  )
+  set(GLIB2_LIBRARIES "${GIO2_LIBRARY} ${GOBJECT2_LIBRARY} ${GLIB2_LIBRARY}")
+
+  find_path(GLIB2_INCLUDE_DIR glib.h
+            HINTS ${_pc_glib_INCLUDE_DIRS}
+            PATH_SUFFIXES glib
+  )
+  set(GLIB2_INCLUDE_DIRS "${GLIB2_INCLUDE_DIR}")
+
+  find_package_handle_standard_args(Glib DEFAULT_MSG GLIB2_LIBRARIES GLIB2_INCLUDE_DIRS)
+endif(GLIB2_FOUND)
+
+endif(GLIB2_INCLUDE_DIRS AND GLIB2_LIBRARIES)
+
+mark_as_advanced(
+  GLIB2_CFLAGS
+  GLIB2_INCLUDE_DIRS
+  GLIB2_LIBRARIES
+)
--- cmake/modules/FindGTK.cmake
+++ cmake/modules/FindGTK.cmake
@@ -10,6 +10,13 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+if(GTK3_INCLUDE_DIRS AND GTK3_LIBRARIES)
+
+  # in cache already
+  set(GTK3_FOUND TRUE)
+
+else(GTK3_INCLUDE_DIRS AND GTK3_LIBRARIES)
+
 include(FindPackageHandleStandardArgs)
 
 if (NOT WIN32)
@@ -19,4 +26,62 @@ if (NOT WIN32)
 
   find_package_handle_standard_args(GTK DEFAULT_MSG GTK3_LIBRARIES GTK3_CFLAGS)
 
+else(NOT WIN32)
+  # assume so, for now
+  set(GTK3_FOUND TRUE)
+
 endif(NOT WIN32)
+
+if(GTK3_FOUND)
+  # set it back as false
+  set(GTK3_FOUND FALSE)
+
+  find_library(GDK3_LIBRARY gdk-3.0
+               HINTS ${_pc_gdk_LIBRARY_DIRS}
+  )
+
+  find_library(GDK3_LIBRARY gdk-3
+               HINTS ${_pc_gdk_LIBRARY_DIRS}
+  )
+
+  find_library(GTK3_LIBRARY gtk-3.0
+               HINTS ${_pc_gtk_LIBRARY_DIRS}
+  )
+
+  find_library(GTK3_LIBRARY gtk-3
+               HINTS ${_pc_gtk_LIBRARY_DIRS}
+  )
+
+  find_library(GDK_PIXBUF_LIBRARY gdk_pixbuf-2.0
+               HINTS ${_pc_gdk-pixbuf_LIBRARY_DIRS}
+  )
+
+  find_library(PANGOCAIRO_LIBRARY pangocairo-1.0
+               HINTS ${_pc_pangocairo_LIBRARY_DIRS}
+  )
+
+  find_library(PANGO_LIBRARY pango-1.0
+               HINTS ${_pc_pango_LIBRARY_DIRS}
+  )
+
+  find_library(ATK_LIBRARY atk-1.0
+               HINTS ${_pc_atk_LIBRARY_DIRS}
+  )
+  set(GTK3_LIBRARIES "${GTK3_LIBRARY} ${GDK3_LIBRARY} ${GDK_PIXBUF_LIBRARY} ${PANGOCAIRO_LIBRARY} ${PANGO_LIBRARY} ${ATK_LIBRARY}")
+
+  find_path(GTK3_INCLUDE_DIR gtk.h
+            HINTS ${_pc_gtk_INCLUDE_DIRS}
+            PATH_SUFFIXES gtk
+  )
+  set(GTK3_INCLUDE_DIRS "${GTK3_INCLUDE_DIR}")
+
+  find_package_handle_standard_args(GTK DEFAULT_MSG GTK3_LIBRARIES GTK3_INCLUDE_DIRS)
+endif(GTK3_FOUND)
+
+endif(GTK3_INCLUDE_DIRS AND GTK3_LIBRARIES)
+
+mark_as_advanced(
+  GTK3_CFLAGS
+  GTK3_INCLUDE_DIRS
+  GTK3_LIBRARIES
+)
